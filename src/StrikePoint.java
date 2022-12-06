f44530a6c6e626dff62715aa94545eaecd022edf
benblack90 <b.schwarz3@ncl.ac.uk>
2022-12-06 12:07:20 +0000

    Updated StrikePoint

--- src/StrikePoint.java
+++ src/StrikePoint.java
@@ -1,12 +1,12 @@
 public class StrikePoint {
     private int x;
     private int y;
-    //hitShip false by default for clarity: must be set when adding to ships
     private boolean hitShip = false;
 
+    private String symbol = "0";
+
     /**Constructor for a StrikePoint. Takes two ints for x (horizontal) and y (vertical) coordinates. hitShip is
-    * determined and set later either by context (if in a ship, hitShip == true), or by comparing a guess with a Ship's
-    * StrikePoint.*/
+    * determined and set later by comparing a guess with a Ship's coordinates. */
     StrikePoint(int x, int y) {
         this.x = x;
         this.y = y;
@@ -20,7 +20,12 @@ public class StrikePoint {
     public void setX(int x) {this.x = x;}
     public void setY(int y) {this.y = y;}
 
-    /**Override of equals, checking equality of X and Y. Ignores hitShip, because likely, that's why equals method is
+    public void updateSymbol(){
+        //Implementation dependent on Ship: is the name of the ship passed in? Do we hold boolean indicating if
+        //StrikePoint is on a ship?
+    }
+
+    /**Override of equals, checking equality of X and Y. Ignores hitShip, because likely, that's what equals method is
      * being used to determine.*/
     @Override
     public boolean equals(Object obj) {
