47355898628ba341a5042e1604be2170ddbe3822
David-Ng <112709900+chikitng@users.noreply.github.com>
2022-12-15 14:40:39 +0000

    Update AI.java

--- src/AI.java
+++ src/AI.java
@@ -1,4 +1,18 @@
 public abstract class AI extends Player {
+   
     //TODO David: this subclass is a placeholder at the moment, as it only has one offshoot: see AdmiralDartboard
-
+        Random rand = new Random();
+        List<Integer> aiGuess = new ArrayList<>();
+        int len = aiGuess.size();
+            public void generateRandomPosition(){
+            for (int i = 0; i < len; i++) {
+                int guessPosition = rand.nextInt(100);
+                if (aiGuess.contains(guessPosition)) {
+                    System.out.println("position duplicate: " + guessPosition);
+                }
+                else {
+                    aiGuess.add(guessPosition);
+                }
+            }
+        }
 }
