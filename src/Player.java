e39516f0f04f52f2e28d7f8e4482d7c81092e902
rachelphwu <rachelphwu@gmail.com>
2023-01-25 16:02:44 +0100

    What have been updated: 1. addShootRecord()
    NoRepeatShoot() is now replaced by this method to add a new point to shootboard if the point has not been shot before. Else, a notification will be printed to ask player to try again.
    
    2a. checkHit()
    
    - Added a comment to respond to Ben's queries.
    
    2b. Lives counter
    
    - Added to the player automatically when the player object is created. Initial value is 5. Every time the player's ship is hit, lives counter minuses 1.
    
    3. checkAllSunk() {New method}
    
    - A new method for game to check if the player has lost the game. This method should be initiated only if checkHit() returns true.
    
    *** Rachel's questions ***
    
    1.
    For checkHit(), who is responsible for updating ship's inSunk status if a ship is sunk? Current design assumes game is responsible.
    
    If player is responsible, I suggest Ship class can change isSunk() to a method allowing easy switch to true rather than StrikePoint input.
    
    2. Is it good to separate checkAllSunk() from checkHit? Current design assumes game is responsible for calling checkAllSunk() when checkHit() returns true.

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,7 @@ public class Player {
     private String name;
     private ArrayList<Ship> shipList = new ArrayList<>();
     private ArrayList<StrikePoint> shootRecord = new ArrayList<>();
-    private int hit;
+    private int lives;
     private Board territoryBoard;
     private Board attackBoard;
 
@@ -15,11 +15,9 @@ public class Player {
         this.attackBoard = attackBoard;
         this.shipList = new ArrayList<>();
         this.shootRecord = new ArrayList<>();
+        lives = 5;
     }
 
-    /*
-    Purpose: avoid repeated name, uppercase, number and symbol
-   */
     public void setName(String newName) {
         name = newName;
     }
@@ -54,7 +52,7 @@ public class Player {
 
     /*
     Purpose: Add ship to the shipList
-    Input: coordinates
+    Input: StrikePoint
     Output: boolean */
 
     //check if the shipList contains the ship
@@ -67,7 +65,7 @@ public class Player {
 
     /*
    Purpose: check against intersection to make sure it is an empty space for placing ship
-   Input: coordinates
+   Input: StrikePoint
    Output: boolean */
 
     //input strikePoint
@@ -83,53 +81,90 @@ public class Player {
 
 
      /*
-  Purpose: check against intersection to make sure it has not been shot before
-  Input: coordinates
-  Output: boolean */
+  Purpose: Make sure the player does not shoot repeatedly before shooting.
+  The new point will be added to the player's shootRecord if the location has not been shot before.
 
-    //input coordinates
-    //return true if
-    public boolean noRepeatedShoot(StrikePoint newPoint) {
+  Input: StrikePoint
+  Output: void
+
+- input newPoint
+- if newPoint equals to a point existing in shootRecord, print out notification and return;
+- else, add the newPoint to shootRecord
+
+  */
+
+    public void addShootRecord(StrikePoint newPoint) {
         /*TODO - I'm guessing this was meant to be a for-each loop, so I've put that in. I also changed the 'else' here
            to print out newPoint. At present, this for-each loop adds a new point to the shootRecord equal to newPoint
            each time it doesn't find a match. The function also needs return statements for each case. I would use this
            to exit the for-each loop and the method as a whole if/when a match is found (false) and return true when a match
            is not found.
         */
-        for (StrikePoint p:
-        shootRecord){
+
+        for (StrikePoint p : shootRecord) {
             //this is
-            if (!newPoint.equals(p)) {
-                shootRecord.add(newPoint);
-            } else {
+            if (newPoint.equals(p)) {
                 System.out.println(newPoint + " has been shot before. Try again.");
+                return;
             }
         }
+        shootRecord.add(p);
     }
+
 
 
     /*Purpose:
-    - Check list of ships against the guesses, pass result to ship class and to board class, update if ship is sunk
-    - With a boolean method to check if game has been won
-   Input: coordinates
+    - Check the player's list of ships against the enemy's guess.
+    - whenever the player got hit, his/her live counter will minus one.
+
+   Input: StrikePoint
    Output: boolean */
 
     /*TODO this method is a bit confused: it declares a winner if a ship receives 5 hits! A player in battleships wins
-    *  when all of his opponent's ships have been sunk. Also, is shipList the player's list or the enemy's? If it's
-    * the players', the player probably shouldn't win if their own ship has been sunk. Returning a String is also not a
-    * good idea: instead we need to modify the StrikePoint present in the ship in question so the boolean for a hit
-    * reads true.*/
-    public String checkHit(StrikePoint p) {
-        for (Ship s : shipList) {
-            if (s.().equals(p)) {
-                hit++;
+     *  when all of his opponent's ships have been sunk. Also, is shipList the player's list or the enemy's? If it's the
+     * players', the player probably shouldn't win if their own ship has been sunk. Returning a String is also not a
+     * good idea: instead we need to modify the StrikePoint present in the ship in question so the boolean for a hit
+     * reads true.
+     * */
 
-                if (hit == 5) {
-                    return "Victory! You hit the last ship and won the game.";
-                }
-                return "Hit";
+    /* Reply from Rachel:
+       The ship list belongs to the player.
+
+       This method is for the player to check enemy's attack against his/her ship list,
+       Game can then pass result to ship class and to board class to update if ship is sunk.
+
+       The hit counter (now changed to lives counter) belongs to the player, not the ship.
+       If a player's lives reduces to 0, it means that all his/her ships are sunk. Game over.
+     */
+
+    public boolean checkHit(StrikePoint p) {
+        for (Ship s : shipList) {
+            if (s.getLocations().equals(p)) {
+                lives--;
+                return true;
             }
         }
-        return "Miss";
+        return false;
+    }
+
+
+/* Purpose:
+ - Check if the game is lost. When lives counter reaches 0,
+   it means all ships are sunk by the enemy and the player has lost the game.
+ - Called by game when checkHit() returns true.
+
+ Input: none
+ Output: boolean
+ */
+
+    public boolean checkAllSunk() {
+        return lives == 0;
     }
 }
+
+
+
+    /*
+    New features to be added later on:
+     Avoid repeated name, uppercase, number and symbol
+   */
